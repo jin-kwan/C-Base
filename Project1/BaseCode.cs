f7ce3f0fd9972911d09a29fd47fc2713986f5766
JK <speedjjk@gmail.com>
2018-07-09 13:55:10 +0900

    commit
    comit

--- Project1/BaseCode.cs
+++ Project1/BaseCode.cs
@@ -12,15 +12,42 @@ namespace JKTest.Code
             //
             StartLog();
 
-            BaseCode code = new BaseCode();
-
-            //code.RefTest();
+            //ref keyword Test
+            TestRef1();
+        
         }
+
+        static void TestRef1() {
+            BaseCode b = new BaseCode();
+            
+            int num = 1;
+            b.AddRef(ref num);
+            Console.WriteLine("ref number:" + num);
+
+            int num2 = 1;
+            b.Add(num2);
+            Console.WriteLine("ref number2:" + num2);
+
+        }
+
 
         static void StartLog() {
 
             Console.WriteLine("================ start ==========================");
 
+        }
+
+        void RefTest() {
+
+
+        }
+
+        void AddRef(ref int num) {
+            num = num + 100;
+        }
+
+        void Add(int num) {
+            num = num + 100;
         }
 
 
