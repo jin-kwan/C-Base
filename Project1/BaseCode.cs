7f62f058ac4c021a814f79c0fbc26d1641e04445
JK <speedjjk@gmail.com>
2018-07-09 14:39:34 +0900

    commit
    commit

--- Project1/BaseCode.cs
+++ Project1/BaseCode.cs
@@ -17,6 +17,7 @@ namespace JKTest.Code
         
         }
 
+        // ref  out 사용법.
         static void TestRef1() {
             BaseCode b = new BaseCode();
             
@@ -28,8 +29,40 @@ namespace JKTest.Code
             b.Add(num2);
             Console.WriteLine("ref number2:" + num2);
 
+            //
+            string f1, f2;
+            b.Split("AAAAAAA BBBBBB CCCCCCCC", out f1, out f2);
+            Console.WriteLine(f1); // AAA... BB..
+            Console.WriteLine(f2); // CC..
+
+            // params 수정자
+            int sum = b.Sum(10, 20, 30, 40);
+            Console.WriteLine("sum:" + sum);
+
+            // 
+            b.Foo(1);       // 1, 0 
+            b.Foo(1, y: 2);     // 1. 2
+            //b.Foo(x: 1, 2);     // 컴파일 오류
         }
 
+        //
+        void Split(string name, out string fName, out string lastName) {
+            int i = name.LastIndexOf(' ');
+            fName = name.Substring(0, i);
+            lastName = name.Substring(i + 1);
+        }
+
+        int Sum(params int[] arr) {
+            int sum = 0;
+            foreach (var a in arr) {
+                sum += a;
+            }
+            return sum;
+        }
+
+        void Foo(int x = 0, int y = 0) {
+            Console.WriteLine(x + ", " + y);
+        }
 
         static void StartLog() {
 
